dd68feed2a402444aaaf1659301da2f836da39e7
foxpp <171215001@qq.com>
2014-11-05 17:32:29 +0800

    测试添加目录及文件

---
+++ HWReader/HWReader/TTT.h
@@ -0,0 +1,13 @@
+//
+//  TTT.h
+//  HWReader
+//
+//  Created by zhaochao on 14-11-5.
+//  Copyright (c) 2014年 huawei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TTT : NSObject
+
+@end
---
+++ HWReader/HWReader/TTT.m
@@ -0,0 +1,13 @@
+//
+//  TTT.m
+//  HWReader
+//
+//  Created by zhaochao on 14-11-5.
+//  Copyright (c) 2014年 huawei. All rights reserved.
+//
+
+#import "TTT.h"
+
+@implementation TTT
+
+@end
