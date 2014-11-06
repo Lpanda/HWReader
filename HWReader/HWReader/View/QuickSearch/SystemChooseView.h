d76277b2cd9c2f7fe07a71cbecfe5f0e5854b813
LPanda <376810857@qq.com>
2014-11-06 16:22:38 +0800

    添加快搜界面xib，完成制式选择子视图

---
+++ HWReader/HWReader/View/QuickSearch/SystemChooseView.h
@@ -0,0 +1,13 @@
+//
+//  SystemChooseView.h
+//  HWReader
+//
+//  Created by LPanda on 14-11-6.
+//  Copyright (c) 2014年 huawei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SystemChooseView : UIView
+
+@end
---
+++ HWReader/HWReader/View/QuickSearch/SystemChooseView.m
@@ -0,0 +1,21 @@
+//
+//  SystemChooseView.m
+//  HWReader
+//
+//  Created by LPanda on 14-11-6.
+//  Copyright (c) 2014年 huawei. All rights reserved.
+//
+
+#import "SystemChooseView.h"
+
+@implementation SystemChooseView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
