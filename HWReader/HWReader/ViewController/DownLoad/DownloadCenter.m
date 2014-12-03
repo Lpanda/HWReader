7a5023f3c5e9c4178505999e8ef5427add304895
LPanda <376810857@qq.com>
2014-12-03 10:14:51 +0800

    添加DownloadCenter

---
+++ HWReader/HWReader/ViewController/DownLoad/DownloadCenter.m
@@ -0,0 +1,13 @@
+//
+//  DownloadCenter.m
+//  HWReader
+//
+//  Created by LPanda on 14-12-2.
+//  Copyright (c) 2014年 huawei. All rights reserved.
+//
+
+#import "DownloadCenter.h"
+
+@implementation DownloadCenter
+
+@end
