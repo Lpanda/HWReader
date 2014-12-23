44b4cfb627da5b25fa4deaaad7bb3ae1df9d4eec
foxpp <171215001@qq.com>
2014-12-23 20:44:13 +0800

    搞定下载进度显示

---
+++ HWReader/HWReader/ViewController/DownLoad/UISwitch+CustomText/UISwitch+CustomText.h
@@ -0,0 +1,13 @@
+//
+//  UISwitch+CustomText.h
+//  HWReader
+//
+//  Created by zhaochao on 14-12-22.
+//  Copyright (c) 2014年 huawei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UISwitch (CustomText)
+
+@end
